6982f6b90040bbf9f6aae0e21b598b6b21a927ef
s99mj <sandhya.gandalwar@justice.gov.uk>
2025-11-27 10:50:28 +0000

    Added python linter check in pipeline

--- san.py
+++ san.py
@@ -1,11 +1,11 @@
 # example.py
-
+import os, sys
 
 def my_function(  x, y ):  # Extra spaces around parameters (E211)
-    print( "Hello, World!" )  # Extra spaces inside parentheses (E201)
+    print(    "Hello, World!" )  # Extra spaces inside parentheses (E201)
     if x>y:  # Missing spaces around operator (E225)
         print(f"{x} is greater than {y}")
-    else:
-       print(f"{x} is less than or equal to {y}")  
+    else:print(f"{x} is less than or equal to {y}")  # Statement on the same line as else (E701)
 
 my_function( 5, 3 )
+
